5e7d21ad4617fe8b14c84bafeef3214288652a15
Janna Lia. Gonzales Cordova <janna@bispace.local>
2026-03-12 17:23:23 -0400

    fixed

--- src/gps/gps.gateway.ts
+++ src/gps/gps.gateway.ts
@@ -1,14 +1,15 @@
 //puertos y configuracion
-import { WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
-import { Server } from "socket.io";
+import { WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 import { GpsData } from './gt06.decoder';
-@WebSocketGateway({ cors: true})
+
+@WebSocketGateway({ cors: true })
 export class GpsGateway {
   @WebSocketServer()
-  server: Server;
+  server!: Server;
 
-  // emite ubicacion a tiempo real a todos los clientes REACT conectados
-  emitLocation(data: GpsData): void{
+  // Emite ubicación en tiempo real a todos los clientes React conectados
+  emitLocation(data: GpsData): void {
     this.server.emit('gps:location', data);
   }
 }
